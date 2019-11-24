90cbc7ff33ff71a62478e10c1bfebb2dfe91f2ef
Mirch~Pirch <mirceamarghitas@gmail.com>
2019-11-24 13:17:53 -0700

    adding newest version

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -20,9 +20,31 @@ export class Card extends React.Component {
                     <img alt="Quote" src={`https://robohash.org/${this.props.monster.id}?set=set3`}/>
                 </div>
                 <p>{this.props.monster.en.substring(0, 60)}</p>
-                <button onClick={this.handleClick} className="cardBtn">READ MORE</button>
+                <button onClick={this.showModal} className="cardBtn">READ MORE</button>
+                <Modal show={this.state.show} handleClose={this.hideModal} >
+                    <h2>{this.props.monster.author}</h2>
+                    <p>{this.props.monster.en}</p>
+                </Modal>
                 <span className={this.props.monster.rating != null ? '' : 'notRated'}>{this.props.monster.rating != null ? this.props.monster.rating : 'Not Rated'}</span>
             </div>
         )
     }
+}
+
+const Modal = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  
+    return (
+      <div className={showHideClassName}>
+        <section className='modal-main'>
+          {children}
+          <button
+            className="closeBtn"
+            onClick={handleClose}
+          >
+            Close
+          </button>
+        </section>
+      </div>
+    );
 }
